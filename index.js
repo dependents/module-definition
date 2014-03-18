9909968fff45abd24bdea4c64fe18f2ae0afb2d4
Joel Kemp <mrjoelkemp@gmail.com>
2014-03-18 18:43:44 -0400

    Using node-source-walk

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-var esprima = require('esprima'),
+var Walker = require('node-source-walk'),
     fs = require('fs');
-
-// From node-detective
-function traverse(node, cb) {
-  if (Array.isArray(node)) {
-    node.forEach(function (x) {
-      if(x != null) {
-        // Mark that the node has been visited
-        x.parent = node;
-        traverse(x, cb);
-      }
-    });
-
-  } else if (node && typeof node === 'object') {
-    cb(node);
-
-    Object.keys(node).forEach(function (key) {
-      // Avoid visited nodes
-      if (key === 'parent' || ! node[key]) return;
-
-      node[key].parent = node;
-      traverse(node[key], cb);
-    });
-  }
-}
-
-// Executes the passed callback for every traversed node of the ast
-function walk(src, cb) {
-  var ast = esprima.parse(src);
-  traverse(ast, cb);
-}
 
 function isExports(node) {
   var c = node.object;
@@ -50,6 +20,8 @@ function isDefine(node) {
 module.exports = function (file, cb) {
   if (! file) throw new Error('filename missing');
 
+  var walker = new Walker();
+
   // Read file
   fs.readFile(file, function (err, data) {
     if (err) {
@@ -58,24 +30,20 @@ module.exports = function (file, cb) {
     }
 
     var src = data.toString(),
-        type = 'none',
-        alreadyDeduced = false;
+        type = 'none';
 
     // Note: this is blocking
-    walk(src, function (node) {
-      if (alreadyDeduced) return;
-
-      // TODO: Figure out how to modify walk such that we can
-      // stop walking once we've deduced the module type
+    walker.walk(src, function (node) {
       if (isExports(node)) {
         type = 'commonjs';
-        alreadyDeduced = true;
+        walker.stopWalking();
+
       } else if (isDefine(node)) {
         type = 'amd';
-        alreadyDeduced = true;
+        walker.stopWalking();
       }
     });
 
-    cb(type);
+    cb && cb(type);
   });
 };
