d989b2b3162e949ec55fdb40c71e62cc72e9f1da
XhmikosR <xhmikosr@gmail.com>
2023-03-18 08:23:52 +0200

    Update minor and patch dependencies

--- test/test.js
+++ test/test.js
@@ -37,7 +37,7 @@ const memfsSample = `
 function testMethodAgainstExpected(method) {
   for (const [file, type] of Object.entries(expected)) {
     method(`./${file}.js`, type);
-  };
+  }
 }
 
 function asyncTest(filename, result) {
